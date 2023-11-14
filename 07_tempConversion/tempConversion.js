b7d3b224435b9d36ca8631ee4f03a093353b4e60
Aleks Marjanovic <alex.marjanovic11@gmail.com>
2023-11-13 20:43:02 -0500

    Finish exercises 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(startTempF) {
+  const tempC = Math.round(((startTempF - 32) * 5/9) * 10) / 10;
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(startTempC) {
+  const tempF = Math.round((startTempC * (9/5) + 32) * 10) / 10;
+  return tempF;
 };
 
 // Do not edit below this line
